238445436ac9032fcfe53dfffe6725efe52c826e
Maria Z <z0559882272@gmail.com>
2025-01-29 20:54:10 +0200

    added xPath

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -24,19 +24,40 @@ public class HomeWork {
 
         WebElement body = wd.findElement(By.tagName("body"));
         WebElement body1 = wd.findElement(By.cssSelector("body"));
+        WebElement body2= wd.findElement(By.xpath("//body"));
+
         WebElement div = wd.findElement(By.tagName("div"));
         WebElement div1 = wd.findElement(By.cssSelector("div"));
+        WebElement div2 = wd.findElement(By.xpath("//div"));
+
+
         WebElement h1 = wd.findElement(By.tagName("h1"));
         WebElement h1_1 = wd.findElement(By.cssSelector("h1"));
+
         List<WebElement> a =wd.findElements(By.tagName("a"));
         List<WebElement> a1 =wd.findElements(By.cssSelector("a"));
 
         //by class
 
+        WebElement el = wd.findElement(By.xpath("//*[@class='container']"));
+
         //by id
 
+        WebElement el1 = wd.findElement(By.xpath("//*[@id = 'root']"));
+
         //by attribute
+        WebElement el2 = wd.findElement(By.xpath("//input[@placeholder='Password']"));
 
         //by attribute --> start & end & contains
+
+        //start
+        WebElement el3 = wd.findElement(By.xpath("//input[starts-with(@placeholder,'Pa')]"));
+
+        //end
+
+
+        //contains
+        WebElement el4 = wd.findElement(By.xpath("//input[contains(@placeholder,'ss')]"));
+
     }
 }
