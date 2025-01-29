0e498aabc1779a5683b3834fd7e061d77e8a4f33
Maria Z <z0559882272@gmail.com>
2025-01-29 20:03:57 +0200

    cssSelectors

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWork {
 
@@ -18,6 +22,21 @@ public class HomeWork {
     public void cssLocators() {
         //by tag name
 
+        WebElement body = wd.findElement(By.tagName("body"));
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
+        WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
+        WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1_1 = wd.findElement(By.cssSelector("h1"));
+        List<WebElement> a =wd.findElements(By.tagName("a"));
+        List<WebElement> a1 =wd.findElements(By.cssSelector("a"));
 
+        //by class
+
+        //by id
+
+        //by attribute
+
+        //by attribute --> start & end & contains
     }
 }
